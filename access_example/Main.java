6c375927242394952e185371361ae388b7e5ab78
Carmen Wretblad <carmen.wretblad@gmail.com>
2026-02-10 17:50:02 +0100

    started some access control tests

---
+++ access_example/Main.java
@@ -0,0 +1,9 @@
+import access_example.UserId;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("test");
+    UserId a = new UserId(130);
+    System.out.println(a);
+  }
+}
+
---
+++ access_example/UserInfo.java
@@ -0,0 +1,8 @@
+
+package access_example;
+import java.util.Optional;
+public class UserInfo {
+  public String firstName;
+  public String lastName;
+  public Optional<String> division;
+}
