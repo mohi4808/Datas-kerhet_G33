3467cdc20c89cf0fbf834ecd0efed5827194bdb6
Carmen Wretblad <carmen.wretblad@gmail.com>
2026-02-12 09:34:54 +0100

    minor structural changes to example

---
+++ access_example/BackendEntry.java
@@ -0,0 +1,31 @@
+import access_example.UserId;
+import access_example.UserInfo;
+import access_example.MedicalRecordEntry;
+import access_example.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class BackendEntry {
+  private List<MedicalRecordEntry> records = new ArrayList<>();
+  private Log log;
+  private HashMap<UserId, UserInfo> users;
+
+public List<MedicalRecordEntry> requestPatientRecords(UserId patient, UserId requestor){
+  // nurse/doctor tries to access -> retrun those in the same division or with them listed in it 
+  // patient tries to access their own -> return their own 
+  // patient tries to access others -> Warning/Denied 
+  return null; 
+}
+public MedicalRecordEntry createNewRecord(UserId creator_id, UserId patient_id, UserId nurse_id){
+  // if doctor -> adds a new entry with them as the doctor under their divisision
+  // else -> rejection / warning; 
+  return null;
+}
+public void deleteRecord(UserId id){
+  // if authority -> yes 
+  // else -> rejection /warning 
+}
+}
